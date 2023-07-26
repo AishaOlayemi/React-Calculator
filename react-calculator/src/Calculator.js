80b9829c003d9098b2f9e74f0e2f7461b3e5e3ca
Aisha <140536347+AishaOlayemi@users.noreply.github.com>
2023-07-26 14:03:07 +0100

    Add files via upload

---
+++ react-calculator/src/Calculator.js
@@ -0,0 +1,56 @@
+import React, { useState } from 'react';
+import './App.css';
+import * as math from 'mathjs';
+
+const Calculator = () => {
+  const [expression, setExpression] = useState('');
+  const [result, setResult] = useState('');
+
+  const handleClick = (value) => {
+    setExpression((prevExpression) => prevExpression + value);
+  };
+
+  const calculateResult = () => {
+    try {
+      const evaluatedResult = math.evaluate(expression);
+      setResult(evaluatedResult);
+    } catch (error) {
+      setResult('Error');
+    }
+  };
+
+  const clearAll = () => {
+    setExpression('');
+    setResult('');
+  };
+
+  return (
+    <div className="calculator">
+      <div className='cal-cont'>
+      <input className="input-box" type="text" value={expression} readOnly />
+      <input className="input-box" type="text" value={result} readOnly />
+      <div className="buttons">
+        <button onClick={() => handleClick('1')}>1</button>
+        <button onClick={() => handleClick('2')}>2</button>
+        <button onClick={() => handleClick('3')}>3</button>
+        <button onClick={() => handleClick('+')}>+</button>
+        <button onClick={() => handleClick('4')}>4</button>
+        <button onClick={() => handleClick('5')}>5</button>
+        <button onClick={() => handleClick('6')}>6</button>
+        <button onClick={() => handleClick('-')}>-</button>
+        <button onClick={() => handleClick('7')}>7</button>
+        <button onClick={() => handleClick('8')}>8</button>
+        <button onClick={() => handleClick('9')}>9</button>
+        <button onClick={() => handleClick('*')}>*</button>
+        <button onClick={() => handleClick('0')}>0</button>
+        <button onClick={() => handleClick('.')}>.</button>
+        <button onClick={() => handleClick('/')}>/</button>
+        <button onClick={calculateResult}>=</button>
+        <button onClick={clearAll}>Clear</button>
+      </div>
+      </div>
+    </div>
+  );
+};
+
+export default Calculator;
